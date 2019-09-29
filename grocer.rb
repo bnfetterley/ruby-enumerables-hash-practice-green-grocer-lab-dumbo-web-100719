a519cc34c9358b124efc5ae2b0a9bbf5e641ce19
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-29 19:11:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -11,7 +11,7 @@ def consolidate_cart(cart)
 element_stats[:count] += 1
       else
         final_hash[element_name] = element_stats
-        final_hash
+        final_hash[:count]
        binding.pry 
    end
   end
