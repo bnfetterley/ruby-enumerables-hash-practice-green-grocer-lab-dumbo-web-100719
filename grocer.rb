ed6a56c1805da3caae857e3822b9e02dae80ac1f
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-29 19:20:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,7 +12,7 @@ element_stats[:count] += 1
       else
         final_hash[element_name] = {
           count: 1,
-          price: 
+          price: element_stats[:price]
         }
       # binding.pry 
    end
