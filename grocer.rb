99d7e5b7712bdd70a00d685dc4d64bee6805b576
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-29 21:09:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,17 +24,16 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon [:item]
-    coupon_item = ["#{item} W/COUPON"] 
     if cart.has_key?(item)
-    if cart[item][:count] >= coupon[:num] && !cart.has_key?(coupon_item)
-      cart[coupon_item] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num]}
+    if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
+      cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num]}
     
     elsif  cart[item][:count] >= coupon[:num] && 
-    cart.has_key?(coupon_item)
-    cart[coupon_item][:count] += coupon[:num]
-    cart[item][:count] -= coupon[:num]
+    cart.has_key?("#{item} W/COUPON")
+    cart["#{item} W/COUPON"][:count] += coupon[:num]
   end
- end
+  cart[item][:count] -= coupon[:num]
+  end
  end
   cart
 end
@@ -52,5 +51,5 @@ def checkout(cart, coupons)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
   total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count]}
-  total > 100 ? total * 0.9 : total
+  total > 100 ? total * 0.9 :
 end
